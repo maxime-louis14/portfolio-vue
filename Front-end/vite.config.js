e287b1312ed8e84f27c0c87fcf30fbc88afa51c5
maxime-louis14 <maximelouis321@gmail.com>
2024-06-25 19:04:56 +0200

    Config vite

--- Front-end/vite.config.js
+++ Front-end/vite.config.js
@@ -3,16 +3,17 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue()], // Utilisation du plugin Vue.js pour Vite
   server: {
     watch: {
-      usePolling: true, // Désactiver le sondage
-      poll: 1000 // Définir un délai de sondage (en millisecondes)
+      usePolling: true, // Activation du polling pour le watch
+      poll: 1000 // Interval de polling en millisecondes (1 seconde)
     },
-    host: true,
-    strictPort: true,
-    port: 3000
+    host: true, // Utilisation de l'hôte par défaut (localhost)
+    strictPort: true, // Port strictement défini (3000)
+    port: 3000 // Port du serveur de développement Vite
   },
-  envPrefix: 'VITE_' // Assurez-vous que les variables d'environnement commencent par 'VITE_'
+  envPrefix: 'VITE_', // Préfixe pour les variables d'environnement spécifiques à Vite
+  exclude: ['node_modules/@vercel/speed-insights/**/*.{ts,tsx}'],
 
 });
