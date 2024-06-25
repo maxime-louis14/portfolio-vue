1327afe0144fb7519f0408b9517c86670a63687a
maxime-louis14 <maximelouis321@gmail.com>
2024-06-25 22:18:07 +0200

    du texte en fr

--- Front-end/src/components/navbar/Navbar.vue
+++ Front-end/src/components/navbar/Navbar.vue
@@ -9,14 +9,14 @@
 
           <div class="flex space-x-4">
             <a href="#Home" class="hover:bg-gray-600 px-4 py-2 rounded">
-              Home
+              Accueil
             </a>
             <a href="#me" class="hover:bg-gray-600 px-4 py-2 rounded"> Profil </a>
             <!-- <a href="#Skills" class="hover:bg-gray-600 px-4 py-2 rounded">
               Skills
             </a> -->
             <a href="#Projects" class="hover:bg-gray-600 px-4 py-2 rounded">
-              Projects
+              Réalisations
             </a>
           </div>
         </div>
